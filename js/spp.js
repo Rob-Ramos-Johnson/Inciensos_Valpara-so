eb6ed75887e70e3bc748675ec913751a14954b8e
Roberto <r.a.ramosjohnson@gmail.com>
2025-08-04 00:37:41 -0400

    Se agrega el proyecto

---
+++ js/spp.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const botones = document.querySelectorAll('button');
+  botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+      alert('Producto añadido al carrito (demo)');
+    });
+  });
+});
